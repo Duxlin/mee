a6f5227783ea931e4149d0cbd568d74162476bda
Duxlin <96833294+Duxlin@users.noreply.github.com>
2023-10-18 01:36:30 +0100

    Add files via upload

--- hwkal.js
+++ hwkal.js
@@ -1,31 +1,30 @@
 // Buat Lu Yang Jual Sc Ini Yang Jujur Jangan Samp Nipu Apalagi Lari Dari Tanggung Jawab
 
-// Base Haikal Hw Mods : Copyan Dari Base Dika
 
 
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.gr = 'https://chat.whatsapp.com/ILl91g45Pw70kibaRNivITt'
-global.ig = '@DUX MD' // ubah aja
+global.gr = 'wa.me/2347082252014'
+global.ig = 'official_quadhir' // ubah aja
 global.email = 'botdux@gmail.com' //serah
 global.region = 'Alien Realm' // serah
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'DUX MD' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Quadhir' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
-global.owner = ['2347082252014'] // ubah aja pake nomor lu
+global.owner = ['2347082252014','46724001843'] // ubah aja pake nomor lu
 //==========================BY Hw Mods=======================//
-global.keyopenai = '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'
+global.keyopenai = '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'
 //====================BY Hw Mods=============================//
-global.botname = 'DUX MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.packname = 'DUX MD' // ubah aja ini nama sticker
-global.author = 'DUX MD' // ubah aja ini nama sticker
-global.prefa = ['','!','.',',','🐤','🗿']
+global.botname = '𝙳𝚄𝚇-𝙱𝙾𝚃' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.packname = '𝙳𝚄𝚇-𝙱𝙾𝚃' // ubah aja ini nama sticker
+global.author = '𝙳𝚄𝚇' // ubah aja ini nama sticker
+global.prefa = ['.']
 global.sessionName = 'haikal' //Gausah Juga
 global.sp = '⭔' // Gausah Juga
 global.wlcm = []
 global.wlcmm = []
-global.anticall = true
+global.anticall = false
 //=================================================//
 //Gausah Juga
 global.limitawal = {
